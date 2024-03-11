c719b67772d75f6b2523ab24a7f541181a326731
Xi Yunhai <xyh20030903xyh@163.com>
2024-03-11 23:23:55 +0800

    update1

--- aatest/mytest.go
+++ aatest/mytest.go
@@ -1,14 +1,15 @@
 package aatest
 
 import (
-	ZeroBot "github.com/wdvxdr1123/ZeroBot"
-	"github.com/wdvxdr1123/ZeroBot/message"
+	zero "github.com/wdvxdr1123/ZeroBot"
 )
 
 func init() {
-	ZeroBot.OnGroupMessage().SetBlock(true).FirstPriority().Handle(func(ctx *ZeroBot.Ctx) {
-		if ctx.Event.GroupID == 925535081 && ctx.Event.Message.String() == "获取密钥" {
-			ctx.SendChain(message.Text("你的密钥是123"))
-		}
+	zero.OnFullMatch("你好").SetBlock(true).FirstPriority().Handle(func(ctx *zero.Ctx) {
+		ctx.Send("你好，我是机器人")
+	})
+
+	zero.OnKeyword("天气").SetBlock(true).FirstPriority().Handle(func(ctx *zero.Ctx) {
+		ctx.Send("今天的天气是晴天")
 	})
 }
